1c478a24e708633b6ce8901e576047bd3b366c37
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-14 01:15:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   lowest_key = ""
   lowest_value = ""
   hash.each do |key, value|
-  if lowest_value = nil || value < lowest_value
+  if lowest_value = "" || value < lowest_value
     lowest_value = value
     lowest_key = key
     end
